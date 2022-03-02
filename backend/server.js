0e6b11d899ec60c8fd4e1af2f5ee844cd61b9567
Raveen Panditha <55187981+RaveenP-glitch@users.noreply.github.com>
2022-03-02 19:56:38 +0530

    server js

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopologyL:true,
+    useFindAndModify:false
+        
+});
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB Connection success!");
+
+})
+
+app.listen(PORT, ()=>{
+    console.log('Server is up and running on port: ${PORT}')
+})
+
+
+
+
